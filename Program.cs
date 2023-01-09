009b6e1b92571ee9a4a303d7ccec6d24edcdf715
Levi Matheus Guerreiro Sange <levi.sange@wisereducacao.com>
2023-01-09 16:59:27 -0300

    1 hora de vídeo .NET

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region TopLevelStatements
+// //Writeline
+// Console.WriteLine("Console: Hello, World!");
+
+// //Statements
+// int i;
+// int i2 = i = 10;
+// int i3 = i2--;
+// i = i2 == 42 ? 10 : 20; //operador ternário
+// Console.WriteLine($"Statements: {i2}");
+
+// //Tipagem estática
+//     //C# é uma linguagem de tipagem estática
+//         //i = i2 == 42 ? 10 : "teste"; //essa expressão não é permitida
+//     // System.//checagem  dp que o namespace system nos oferece
+//     // <Aviso> TIPOS ESTÁTICOS NÃO PERMITEM CRIAÇÃO DE OBJETOS, TIPOS ESTÁTICOS SÓ CONTÉM FUNCIONALIDADES </Aviso>
+//         // <Aviso> TODOS MEMBROS DO TIPO ESTÁTICO TAMBÉM PRECISAM SER ESTÁTICOS </Aviso>
+//         //Membros do tipo estático podem ser utilizados chamando pelo .
+//             //Exemplo
+//             //string. membros estáticos da classe string
+
+// //Um programa simples de interação com Console
+// Console.Write("Digite seu nome: ");
+// string name = Console.ReadLine();
+// System.Console.WriteLine($"Olá {name}");
+// System.Console.WriteLine("Digite o ano do seu nascimento: ");
+// int year = int.Parse(Console.ReadLine());
+// int age = 2022 - year;
+// System.Console.WriteLine($"Você tem {age} anos.");
+
+// //Condicionais
+// if(age >= 18) System.Console.WriteLine("Você é maior da idade.");
+// else System.Console.WriteLine("Você é menor de idade");
+#endregion
+
+public class Program{
+    static void Main()
+    {
+        //Arrays
+        string[] nomes = {  "Levi", "Maria" };
+        int j = new int();
+    }
+}
