e8f95aa962c94f56e3e8eeda68c52920e68f73d8
Levi Sange <levi.matheusmartins@gmail.com>
2023-01-30 20:52:24 -0300

    S04EP04 - Modelo Contato

---
+++ Basico_EntityFramework/Comercio.Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comercio.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Sexo { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }//Datetime com localização
+
+        public Contato? Contato { get; set;}
+
+        /*
+         * TODO - Vincular com as classes
+         * - Contato
+         * - EnderecoEntrega
+         * - Departamento
+         * */
+    }
+}
