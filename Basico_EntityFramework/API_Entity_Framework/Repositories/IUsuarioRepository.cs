d5175a1290552b39c7b559fa250d35fd91cee0d8
Levi Sange <levi.matheusmartins@gmail.com>
2023-01-31 11:04:32 -0300

    S04EP09 Implementação do UsuarioRepository

---
+++ Basico_EntityFramework/API_Entity_Framework/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,20 @@
+﻿using Comercio.Models;
+
+namespace Comercio.API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        /* CRUD - Criar, Ler, Atualizar e Deletar */
+
+        //Listar usuarios
+        List<Usuario> get();
+        //Buscar um usuário específico
+        Usuario Get(int id);
+        //Adicionar um usuário
+        void Add(Usuario usuario);
+        //Atualizar um usuário
+        void Update(Usuario usuario);
+        //Excluir um usuário
+        void Delete(int id);
+    }
+}
