4c6362bdcdfe8e03c03a58f62913d05144ba4472
Levi Sange <levi.matheusmartins@gmail.com>
2023-02-01 20:46:20 -0300

    Criado a string de conexão

---
+++ Basico_EntityFramework/API_Entity_Framework/Database/eCommerceContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Comercio.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        /* 
+         Conexão sem distinção de ambientes de execução
+         */
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Indicando qual banco vai ser utilizado (SqlServer)
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True");
+        }
+    }
+}
