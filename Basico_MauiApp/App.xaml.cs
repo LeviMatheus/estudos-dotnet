7ee610e6b3f8d7721d62b50037b04fb74004fde3
Levi Sange <levi.matheusmartins@gmail.com>
2023-02-02 17:30:35 -0300

    Inclusão inicial projeto .net maui

---
+++ Basico_MauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Basico_MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Basico_MauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Basico_MauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
